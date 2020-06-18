5cec7b97edd7f3fada2159d039f40e019dfa972e
AliDoesPhysics <ali-al-ali@live.co.uk>
2020-06-18 16:56:02 +0100

    Update sproject_prelim.py

--- sproject_prelim.py
+++ sproject_prelim.py
@@ -29,12 +29,12 @@ m_p_array = m_p*unit_mass
 ### random dists
 # For random samples from N(\mu, \sigma^2), use:
 #sigma * np.random.randn(...) + mu
-pt_gauss1 = np.random.normal(0.8, 0.3, size=1000)
-eta_gauss1 = np.random.normal(1, 0.5, size=1000)
+pt_gauss1 = np.random.normal(200, 1, size=1000)
+eta_gauss1 = np.random.normal(2, 0.2, size=1000)
 phi_uniform= np.random.uniform(2*np.pi, size = 1000)
 
-pt_gauss2 = np.random.normal(0.7, 0.25, size=1000)
-eta_gauss2 = np.random.normal(0.9, 0.1, size=1000)
+pt_gauss2 = np.random.normal(203, 1.5, size=1000)
+eta_gauss2 = np.random.normal(3.5, 0.3, size=1000)
 phi_uniform= np.random.uniform(2*np.pi, size = 1000)
 
 pt_po = np.random.poisson(1000, 1000)
@@ -55,7 +55,7 @@ gauss_gun2 = gauss_gun(unit_mass, pt_gauss2, eta_gauss2, phi_uniform)   #contain
 def inv_m(fmtm): #must be a 4 vector (m, pt, eta, phi), and is in natural units
     energy = fmtm[:,0]
     mtm_mag = fmtm[:,1]*np.cosh(fmtm[:,2])
-    inv = energy**2 - mtm_mag**2 ### once you have proper data ADD SQRT
+    inv = np.sqrt(np.abs(energy**2 - mtm_mag**2)) ### once you have proper data ADD SQRT (added abs to circumvent)
     return inv
 
 invm_gun1 = inv_m(gauss_gun1)
@@ -65,8 +65,8 @@ invm_gun2 = inv_m(gauss_gun2)
                   
 #%% plot gun histograms
 
-plt.hist(invm_gun1, bins=40, alpha=0.5, label = "Gaussian Gun 1")
-plt.hist(invm_gun2, bins=40, alpha=0.5, label = "Gaussian Gun 2")
+plt.hist(invm_gun1, bins=300, alpha=0.5, label = "Gaussian Gun 1")
+plt.hist(invm_gun2, bins=300, alpha=0.5, label = "Gaussian Gun 2")
 plt.xlabel("Invariant Mass")
 plt.ylabel("Number")
 plt.legend(loc = "best")
@@ -83,5 +83,25 @@ var_gun2 = np.var(invm_gun2)
 
 fisher_discriminant = (mean_gun1 - mean_gun2)**2 / (var_gun1 + var_gun2)
 
+#%% use make_blobs to generate data
+
+X, y = make_blobs(n_samples=100)
+# X stores the data as a column vector and y stores class labels for each sample as an integer
+# returns 2 columns, each col with vals for a class
+# y values will all be 1 or 2 as n_features = 2 means there are 2 classes
+
+#%% plot blobs
+
+def plot_blobs(X,y):
+    df = pd.DataFrame(dict(x=X[:,0],y=X[:,1], label=y)) #can use dict as have only 2 classes
+    # label is y as y contains the classes
+    colours = {0: "red", 1:"green", 2:"blue"} #dictionary relating class labels to plot colours
+    fig, ax = plt.subplots(figsize=(12,8))
+
+    grouped = df.groupby('label') #to make plot, we need to group datapoints according to their class (=label)
+    for key, group in grouped:
+        group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colours[key], s=10) #x and y are strings since they are the names of df columns
+    
+    plt.show()
 
 
