e0baa046e47a08413c28bb91e0e0d5481e401e9a
AliDoesPhysics <ali-al-ali@live.co.uk>
2020-06-21 16:03:34 +0100

    Update ROC_curves.py

--- ROC_curves.py
+++ ROC_curves.py
@@ -5,4 +5,15 @@ Created on Sun Jun 21 14:55:18 2020
 
 @author: AliAlAli
 """
+import torch
+from sklearn.metrics import roc_curve, roc_auc_score
+#%% getting actual data from lili
+from Electron_ID_data import file, output_data
 
+#%%getting Kevin LDA data
+
+from LDA_numpy import setosa, versicolor, setosa_t, versicolor_t
+
+#%% test on Kevin's LDA
+
+setosa_auc = roc_auc_score(setosa, setosa_t )
