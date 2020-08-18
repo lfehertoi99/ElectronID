922c42ea538085bab4dcd064e44169e95a49169e
Lili Fehertoi <lfehertoi@gmail.com>
2020-08-18 14:46:48 +0200

    Update Kinematics_LDA_MaxLhood.py

--- Kinematics_LDA_MaxLhood.py
+++ Kinematics_LDA_MaxLhood.py
@@ -14,82 +14,76 @@ from scipy import stats
 from scipy.stats import multivariate_normal
 from scipy.stats import norm
 from scipy.stats import uniform
-from scipy.stats import poisson
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.decomposition import PCA
 from sklearn.datasets import make_blobs
 #%%
 #GENERATING PARTICLES FROM MC GUNS WITH DIFFERENT (GAUSSIAN) PSEUDORAPIDITY AND TRANSVERSE MOMENTUM DISTRIBUTIONS
-def g_dist(pt, eta, phi, m): #all arguments should be vectors!
-    g_fmtm = np.column_stack((pt, eta, phi, m))
-    return g_fmtm
+class BinaryParticleGun:
     
-normal = lambda x,y: norm(x,y)
-uniform_d = lambda x,y: uniform(loc=x, scale=y)
+    def __init__(self, p_range = [[[175, 225], [1.2, 2.2]], [[1.7, 4], [0.25, 0.55]]], size = 1000, mass = 1):
+        self.p_range = p_range #stores range of possible parameters
+        self.size = size
+        self.mass = mass
+        
+    def get_dist(self):
+        normal = lambda x,y: norm(x,y).rvs(size = self.size)
+        pt_mean, pt_std = np.random.uniform(*self.p_range[0][0]), np.random.uniform(*self.p_range[0][1])
+        eta_mean, eta_std = np.random.uniform(*self.p_range[1][0]), np.random.uniform(*self.p_range[1][1])
+        pt_dist, eta_dist = normal(pt_mean, pt_std), normal(eta_mean, eta_std)
+        return [[pt_mean, eta_mean], [pt_dist, eta_dist]]
 
-g1_eta = normal(2, 0.2).rvs(size=1000)
-g2_eta = normal(3.5, 0.3).rvs(size=1000)
-
-
-g1_pt = normal(200, 1).rvs(size=1000)
-g2_pt = normal(203, 1.5).rvs(size=1000)
-
-g1_phi = uniform_d(0, 2*np.pi).rvs(size=1000)
-g2_phi = uniform_d(0, 2*np.pi).rvs(size=1000)
-
-mass = np.ones(1000)
+    def get_fmtm(self, pt, eta, phi_loc = 0, mass = 1): #all arguments should be vectors!
+        phi = uniform(loc = phi_loc, scale = 2*np.pi).rvs(size = self.size)
+        m = np.array([mass] * self.size)
+        g_fmtm = np.column_stack((pt, eta, phi, m))
+        return g_fmtm
+    
+    def get_samples(self):
+        g1_params, g1_vars = self.get_dist()
+        g2_params, g2_vars = self.get_dist()
+        g1_data, g2_data = self.get_fmtm(*g1_vars), self.get_fmtm(*g2_vars)
+        samples = np.concatenate((g1_data, g2_data))
+        labels = np.concatenate((np.zeros(self.size), np.ones(self.size)))
+        return np.column_stack((samples, labels))
+        
+    def split(self, data, ratio):
+        bitmask = np.random.randn(len(data)) < ratio
+        train = data[bitmask]
+        test = data[~bitmask]
+        return train, test
 #%%
-experiment1, experiment2 = g_dist(g1_pt, g1_eta, g1_phi, mass), g_dist(g2_pt, g2_eta, g2_phi, mass) #4-momentum vectors from guns
-#CREATE PLOTS OF DATA IN p_t - eta space
-fig, ax = plt.subplots(1, 1, figsize=(12,8))
-
-ax.scatter(g1_pt, g1_eta, c = "r", label = "Gun 1", s = 10)
-ax.scatter(g2_pt, g2_eta, c = "g", label = "Gun 2", s = 10)
-
-ax.legend()
+particlegun = BinaryParticleGun()
+labeled_data = particlegun.get_samples()
+train, test = particlegun.split(labeled_data, 0.75)
+#%%
+fig, ax = plt.subplots(figsize=(12,8))
+for l,c,m in zip(np.unique(train[:, 4]),['r','g'],['o','x']): #"zip" creates iterator that pairs together values in respective tuples (sp.unique(y) contains class labels)
+    ax.scatter(train[:, :2][train[:, 4]==l,0],
+                train[:, :2][train[:, 4]==l,1],
+                c=c, marker=m, label=l, s=10)
+    
+ax.set_title("Distribution of kinematic variables")
 ax.set_xlabel("Transverse Momentum")
 ax.set_ylabel("Pseudorapidity")
-
+plt.legend()
 plt.show()
-#%%
-#CREATE CLASS LABELS FOR DATA
-g1_labels = np.zeros(1000)
-g2_labels = np.ones(1000)
-
-pt_vals = np.concatenate((g1_pt, g2_pt))
-eta_vals = np.concatenate((g1_eta, g2_eta))
-labels = np.concatenate((g1_labels, g2_labels))
-
-pt_eta = np.column_stack((pt_vals, eta_vals, labels))
-
-#SPLIT DATA INTO TRAINING AND TEST
-bitmask = np.random.randn(len(pt_eta)) < 0.7
-train = pt_eta[bitmask]
-test = pt_eta[~bitmask]
-#%%
-#TRIAL RUN OF LDA IN SKLEARN WITH RANDOMLY GENERATED DATA
-X, y = make_blobs(n_samples=200, centers=3, n_features=2) #X contains a column vector for each feature, y contains integer class labels for each sample
-lineardisc = LDA(n_components = 2) #OBJECT of class LDA with attribute n_components set to 2
-lineartrans = lineardisc.fit(X[:150], y[:150]) #fit model to randomly generated blobs (75%)
-accuracy = lineardisc.score(X[150:], y[150:]) #predict and assess accuracy (25%)
-print(accuracy) #print accuracy
 #%%
 #LDA for data in p_t - eta space, using sklearn
 pt_eta_lda = LDA(n_components = 2) #OBJECT of class LDA with attribute n_components set to 2
-pt_eta_lda.fit(train[:, :2], train[:, 2]) #finds best decision boundary based on maximizing inter-class and minimizing intra-class variance
+pt_eta_lda.fit(train[:, :2], train[:, 4]) #finds best decision boundary based on maximizing inter-class and minimizing intra-class variance
 
-train_accuracy = pt_eta_lda.score(train[:, :2], train[:, 2])
-test_accuracy = pt_eta_lda.score(test[:, :2], test[:, 2])
+train_accuracy = pt_eta_lda.score(train[:, :2], train[:, 4])
+test_accuracy = pt_eta_lda.score(test[:, :2], test[:, 4])
 print(train_accuracy, test_accuracy)
 #%%
 # Plot the hyperplanes and datapoints (including test data)
 fig, ax = plt.subplots(figsize=(12,8))
-for l,c,m in zip(np.unique(train[:, 2]),['r','g'],['o','x']): #"zip" creates iterator that pairs together values in respective tuples (sp.unique(y) contains class labels)
-    ax.scatter(pt_eta[:, :2][pt_eta[:, 2]==l,0],
-                pt_eta[:, :2][pt_eta[:, 2]==l,1],
+for l,c,m in zip(np.unique(train[:, 4]),['r','g'],['o','x']): #"zip" creates iterator that pairs together values in respective tuples (sp.unique(y) contains class labels)
+    ax.scatter(train[:, :2][train[:, 4]==l,0],
+                train[:, :2][train[:, 4]==l,1],
                 c=c, marker=m, label=l, s=10)
 
-x1 = np.array([np.amin(pt_eta[:,0], axis=0), np.amax(pt_eta[:,0], axis=0)])
+x1 = np.array([np.amin(train[:,0], axis=0), np.amax(train[:,0], axis=0)])
 
 #lda.coef_ contains coefficients of decision boundary vectors
 b, w1, w2 = pt_eta_lda.intercept_[0], pt_eta_lda.coef_[0][0], pt_eta_lda.coef_[0][1]
@@ -116,7 +110,7 @@ class ParticleNaiveBayes:
     
     #do MLE and get parameters for model boased on training data
     def get_params(self, train):
-        train_g1, train_g2 = train[train[:, 2] == 0], train[train[:, 2] == 1] #training data contains pt data in column 1, eta data in column 2
+        train_g1, train_g2 = train[train[:, 4] == 0], train[train[:, 4] == 1] #training data contains pt data in column 1, eta data in column 2
         g1_pt_params, g2_pt_params = self.MLE(train_g1[:, 0]), self.MLE(train_g2[:, 0])
         g1_eta_params, g2_eta_params = self.MLE(train_g1[:, 1]), self.MLE(train_g2[:, 1])
         self.params = [[g1_pt_params, g2_pt_params], [g1_eta_params, g2_eta_params]]
@@ -161,7 +155,7 @@ plt.show()
 fig, ax = plt.subplots(figsize=(12,8))
 
 ax.scatter(test[:, 0], test[:, 1], c = classes, s = 8)
-ax.scatter(train[:, 0], train[:, 1], c = train[:, 2], s = 8)
+ax.scatter(train[:, 0], train[:, 1], c = train[:, 4], s = 8)
 
 ax.set_title("Naive Bayes classifier performance")
 ax.set_xlabel("Transverse momentum")
