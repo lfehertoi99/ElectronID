0f333786b2331583f4fb5124e2c0ce2a3917636f
Lili Fehertoi <lfehertoi@gmail.com>
2020-08-24 22:22:25 +0200

    Update NN_vanilla.py

--- NN_vanilla.py
+++ NN_vanilla.py
@@ -13,6 +13,7 @@ input_data = sp.random.randn(10, 1)
 output_data = sp.random.randn(5, 1)
 #%%
 threshold = 0.5
+max_iter = 100000
 learning_rate = 0.000001
 #%%
 weights1 = sp.random.normal(size = (10, 8))
@@ -20,15 +21,16 @@ weights2 = sp.random.normal(size = (8, 5))
 #%%
 loss_data = []
 #%%
-loss = 100
-while loss >= threshold:
+i = 0
+loss = threshold
+while loss >= threshold and i < max_iter:
     
-        z1 = sp.dot(weights1.T, input_data) #8 x 1
-        a1 = sp.maximum(0, z1) #8 x 1
-        z2 = sp.dot(weights2.T, a1) #5 x 1
-        a2 = sp.maximum(0, z2) #5 x 1
+        z1 = sp.dot(weights1.T, input_data) #8 x 1 linear transformation no.1
+        a1 = sp.maximum(0, z1) #8 x 1 #relu no.1
+        z2 = sp.dot(weights2.T, a1) #5 x 1  linear transformation no. 2
+        a2 = sp.maximum(0, z2) #5 x 1 relu no.2 
         
-        loss = 1/len(output_data) * sp.sum((a2 - output_data)**2)
+        loss = 1/len(output_data) * sp.sum((a2 - output_data)**2) #mean squared error
         loss_data.append(loss)
         
         grad_a2 = 2 * (a2 - output_data) #gradient w.r.t y_hat,dim n x 1
@@ -40,6 +42,8 @@ while loss >= threshold:
         
         weights1 -= learning_rate * grad_w1
         weights2 -= learning_rate * grad_w2
+        
+        i += 1
 #%%
 iterations = sp.arange(0, len(loss_data), 1)
 #%%
